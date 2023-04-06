5add8d4a6f602c3310ed2d708d393d3c4b7e06bb
Elena <lenok791@mail.ru>
2023-04-06 20:30:34 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i;
         float operand1 = (float) scanner.nextInt();
         while (true) {
             operand1 = Calculator.calOperation(scanner, operand1);
-     
+
         }
     }
 }
